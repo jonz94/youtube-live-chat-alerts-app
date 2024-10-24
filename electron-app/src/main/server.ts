7bb41b5a8f1ac2886df282389ca1d5879a3c66cf
jonz94 <jody16888@gmail.com>
2024-10-24 21:08:51 +0800

    feat: setup `socket.io`

--- electron-app/src/main/server.ts
+++ electron-app/src/main/server.ts
@@ -2,9 +2,14 @@ import { is } from '@electron-toolkit/utils'
 import { serve } from '@hono/node-server'
 import { serveStatic } from '@hono/node-server/serve-static'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { type Server as HttpServer } from 'node:http'
+import { io, startLivechat, startWebsocket } from './websocket'
 
 export function startWebServer(port = 21829) {
   const app = new Hono()
+
+  app.use('/*', cors())
 
   app.use(
     '/overlays/*',
@@ -28,10 +33,24 @@ export function startWebServer(port = 21829) {
     return c.text('OK')
   })
 
+  app.get('/api/live-chat/:videoId', (c) => {
+    const { videoId } = c.req.param()
+
+    if (!io || !videoId) {
+      return c.json({ error: 'no websocker connection' }, 400)
+    }
+
+    startLivechat(videoId)
+
+    return c.json({ videoId })
+  })
+
   console.log(`Server is running on port ${port}`)
 
-  serve({
+  const server = serve({
     fetch: app.fetch,
     port,
   })
+
+  startWebsocket(server as HttpServer)
 }
