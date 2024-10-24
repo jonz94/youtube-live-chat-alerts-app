da4be44fb2f513bb9920aac0b3a970efb3e61bc3
jonz94 <jody16888@gmail.com>
2024-10-24 21:09:32 +0800

    chore: remove video info component

--- electron-app/src/main/api.ts
+++ electron-app/src/main/api.ts
@@ -1,30 +1,9 @@
 import { initTRPC } from '@trpc/server'
 import { join } from 'node:path'
-import { Innertube } from 'youtubei.js'
-import z from 'zod'
 
 const t = initTRPC.create({ isServer: true })
 
 export const router = t.router({
-  getVideoBasicInfo: t.procedure.input(z.object({ videoId: z.string() })).query(async ({ input }) => {
-    const youtube = await Innertube.create()
-    const video = await youtube.getBasicInfo(input.videoId)
-
-    const { is_live, is_upcoming, title, start_timestamp, end_timestamp, duration } = video.basic_info
-
-    const info = {
-      isLive: is_live,
-      isUpcoming: is_upcoming,
-      title,
-      startTimestamp: start_timestamp?.toISOString(),
-      endTimestamp: end_timestamp?.toISOString(),
-      duration,
-    }
-
-    console.log(info)
-
-    return info
-  }),
   debug: t.procedure.query(() => {
     const cwd = process.cwd()
     const dirname = __dirname
--- electron-app/src/renderer/src/App.tsx
+++ electron-app/src/renderer/src/App.tsx
@@ -1,13 +1,10 @@
 import Debug from '~/renderer/components/debug'
 import { ModeToggle } from '~/renderer/components/mode-toggle'
 import Versions from '~/renderer/components/versions'
-import VideoInfo from '~/renderer/components/video-info'
 
 export default function App() {
   return (
     <div className="min-h-screen grid place-content-center">
-      <VideoInfo></VideoInfo>
-
       <ModeToggle></ModeToggle>
 
       <Debug></Debug>
