f834551f21f4554442c80d167dc8fe9234c8728d
jonz94 <jody16888@gmail.com>
2024-11-05 18:43:30 +0800

    build: ignore chunk size warning limit for electron renderer

--- apps/youtube-overlays-app/electron.vite.config.ts
+++ apps/youtube-overlays-app/electron.vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()],
   },
   renderer: {
+    build: {
+      chunkSizeWarningLimit: Infinity,
+    },
     resolve: {
       alias: {
         '~/renderer': resolve(import.meta.dirname, './src/renderer/src'),
