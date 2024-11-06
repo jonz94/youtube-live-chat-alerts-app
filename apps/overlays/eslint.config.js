a145c1e938a430ac5686416ad0089a0dc0082878
jonz94 <jody16888@gmail.com>
2024-11-06 21:13:14 +0800

    chore: remove `eslint-plugin-react-refresh`

--- apps/overlays/eslint.config.js
+++ apps/overlays/eslint.config.js
@@ -1,7 +1,6 @@
 import js from '@eslint/js'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
@@ -22,13 +21,11 @@ export default tseslint.config(
     plugins: {
       react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-confusing-void-expression': ['error', { ignoreArrowShorthand: true }],
     },
   },
