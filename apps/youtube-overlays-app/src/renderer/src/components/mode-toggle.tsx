db5cfc49ec852e886ea9d549dc21890c21c4dca1
jonz94 <jody16888@gmail.com>
2025-01-06 12:51:11 +0800

    feat: add tooltip for `<ModeToggle>`

--- apps/youtube-overlays-app/src/renderer/src/components/mode-toggle.tsx
+++ apps/youtube-overlays-app/src/renderer/src/components/mode-toggle.tsx
@@ -1,5 +1,5 @@
 import { Moon, Sun } from 'lucide-react'
-import { useTheme } from '~/renderer/components/theme-provider'
+import { type Theme, useTheme } from '~/renderer/components/theme-provider'
 import { Button } from '~/renderer/components/ui/button'
 import {
   DropdownMenu,
@@ -7,29 +7,48 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '~/renderer/components/ui/dropdown-menu'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '~/renderer/components/ui/tooltip'
+
+const THEME_DISPLAY_NAME_LOOKUP_TABLE = {
+  light: '亮色主題',
+  dark: '暗色主題',
+  system: '系統預設',
+} as const satisfies Record<Theme, string>
+
+const THEMES = Object.keys(THEME_DISPLAY_NAME_LOOKUP_TABLE) as Theme[]
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { theme: currentTheme, setTheme } = useTheme()
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
+      <TooltipProvider delayDuration={250}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+          </TooltipTrigger>
+
+          <TooltipContent>
+            <div className="flex flex-col justify-center items-center gap-y-1">
+              <p>切換亮色主題 / 暗色主題</p>
+              <p>目前設定：{THEME_DISPLAY_NAME_LOOKUP_TABLE[currentTheme]}</p>
+            </div>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
       <DropdownMenuContent align="end">
-        <DropdownMenuItem className="text-lg" onClick={() => setTheme('light')}>
-          亮色主題
-        </DropdownMenuItem>
-        <DropdownMenuItem className="text-lg" onClick={() => setTheme('dark')}>
-          暗色主題
-        </DropdownMenuItem>
-        <DropdownMenuItem className="text-lg" onClick={() => setTheme('system')}>
-          系統預設
-        </DropdownMenuItem>
+        {THEMES.map((theme) => (
+          <DropdownMenuItem key={theme} className="text-lg" onClick={() => setTheme(theme)}>
+            {THEME_DISPLAY_NAME_LOOKUP_TABLE[theme]}
+          </DropdownMenuItem>
+        ))}
       </DropdownMenuContent>
     </DropdownMenu>
   )
